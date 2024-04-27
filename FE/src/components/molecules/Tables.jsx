0086dd8a1160d23317cb81c56468708440da650f
Numenor08 <athaya120@gmail.com>
2024-04-28 03:34:40 +0700

    fix insight api

--- FE/src/components/molecules/Tables.jsx
+++ FE/src/components/molecules/Tables.jsx
@@ -11,22 +11,22 @@ import {
 import { Button } from "@/components/ui/button";
 import { Pencil1Icon, EraserIcon } from "@radix-ui/react-icons";
 
-const invoices = [
-	{
-		id: 1,
-		labaRugi: false,
-		nominal: 10000,
-		tanggal: "2022-11-17",
-		user: "test abc"
-	},
-	{
-		id: 2,
-		labaRugi: true,
-		nominal: 10000,
-		tanggal: "2022-11-17",
-		user: "test abc"
-	}
-];
+// const invoices = [
+// 	{
+// 		id: 1,
+// 		labaRugi: false,
+// 		nominal: 10000,
+// 		tanggal: "2022-11-17",
+// 		user: "test abc"
+// 	},
+// 	{
+// 		id: 2,
+// 		labaRugi: true,
+// 		nominal: 10000,
+// 		tanggal: "2022-11-17",
+// 		user: "test abc"
+// 	}
+// ];
 
 export default function Tables({ data }) {
 	return (
@@ -43,19 +43,32 @@ export default function Tables({ data }) {
 			</TableHeader>
 
 			<TableBody>
-				{invoices.map((invoice) => (
-					<TableRow key={invoice.id}>
-						<TableCell className="font-medium">{invoice.labaRugi ? "Laba" : "Rugi"}</TableCell>
-						<TableCell className="font-medium">{invoice.nominal} Kg</TableCell>
-						<TableCell>{invoice.tanggal}</TableCell>
-						<TableCell>{invoice.user}</TableCell>
+				{data.length !== 0 ? (data.map((data) => (
+					<TableRow key={data.id}>
+						<TableCell className="font-medium">{data.labaRugi ? "Laba" : "Rugi"}</TableCell>
+						<TableCell className="font-medium">{data.nominal} Kg</TableCell>
+						<TableCell>{new Date(data.created_at).toLocaleDateString()}</TableCell>
+						<TableCell>{data.author.nama}</TableCell>
 						<TableCell className="flex gap-4">
 							<Button variant="outline" size="icon" className="w-8 h-8 border-green-500">
 								<EraserIcon />
 							</Button>
 						</TableCell>
 					</TableRow>
-				))}
+				))) : (
+					<TableRow key={data.id}>
+						<TableCell>Kosong</TableCell>
+						<TableCell>Kosong</TableCell>
+						<TableCell>Kosong</TableCell>
+						<TableCell>Kosong</TableCell>
+						<TableCell className="flex gap-4">
+							{/* <Button variant="outline" size="icon" className="w-8 h-8 border-green-500">
+								<EraserIcon />
+							</Button> */}
+						</TableCell>
+					</TableRow>
+					
+				)}
 			</TableBody>
 		</Table>
 	);
--- FE/src/pages/Insight.jsx
+++ FE/src/pages/Insight.jsx
@@ -1,129 +1,138 @@
 import Navbar from "@/components/organism/Navbar";
 import { PlusIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Tables from "@/components/molecules/Tables";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import InsightCard from "@/components/molecules/InsightCard";
+import { useGet } from "@/hooks/useFetch";
 
 const sty = {
-	Card: "mb-4 md:mx-20 lg:mx-40",
-	Button: "w-8 h-8 border-2 border-green-700 hover:bg-green-700 hover:text-slate-200"
+  Card: "mb-4 md:mx-20 lg:mx-40",
+  Button:
+    "w-8 h-8 border-2 border-green-700 hover:bg-green-700 hover:text-slate-200",
 };
 
 export default function Insight() {
-	// fetch("http://127.0.0.1:8000/api/history/store", {
-	// 	method: "POST",
-	// 	header: { "Content-Type": "application/json" },
-	// 	body: JSON.stringify({
-	// 		user_id: 2,
-	// 		tipe_histori: 1,
-	// 		tipe_sampah: "kompos",
-	// 		nominal: 10
-	// 	})
-	// }).then((res) => console.log(res));
+  const { data } = useGet("http://127.0.0.1:8000/api/history/");
+  const kompos = data.data.filter(
+    (item) => item.tipe_sampah.toLowerCase() === "kompos"
+  );
+  const organik = data.data.filter(
+    (item) => item.tipe_sampah.toLowerCase() === "organik"
+  );
+  const anorganik = data.data.filter(
+    (item) => item.tipe_sampah.toLowerCase() === "anorganik"
+  );
+  return (
+    <>
+      <Navbar />
 
-	return (
-		<>
-			<Navbar />
+      <div className="w-screen min-h-screen pt-24 pb-8 px-8 bg-slate-200">
+        <Tabs defaultValue="kompos" className="">
+          <TabsList className="grid w-full grid-cols-3">
+            <TabsTrigger value="kompos">Kompos</TabsTrigger>
+            <TabsTrigger value="organik">Organik</TabsTrigger>
+            <TabsTrigger value="anorganik">Anorganik</TabsTrigger>
+          </TabsList>
 
-			<div className="w-screen min-h-screen pt-24 pb-8 px-8 bg-slate-200">
-				<Tabs defaultValue="kompos" className="">
-					<TabsList className="grid w-full grid-cols-3">
-						<TabsTrigger value="kompos">Kompos</TabsTrigger>
-						<TabsTrigger value="organik">Organik</TabsTrigger>
-						<TabsTrigger value="anorganik">Anorganik</TabsTrigger>
-					</TabsList>
+          {/* KOMPOS */}
+          <TabsContent value="kompos">
+            <Card className={sty.Card}>
+              <CardHeader className="grid grid-cols-2">
+                <div>
+                  <CardTitle>Kompos</CardTitle>
+                  <CardDescription className="mt-4">
+                    <p>{`Total Pengeluaran: ${data.totalPengeluaran}`}</p>
+                    <p>{`Total Pendapatan: ${data.totalPendapatan}`}</p>
+                    <p>{`Total Kompos: ${data.totalKompos}`}</p>
+                  </CardDescription>
+                </div>
+                <div className="flex justify-end gap-4">
+                  <Button variant="outline" size="icon" className={sty.Button}>
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <PlusIcon />
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <InsightCard>Kompos</InsightCard>
+                      </DialogContent>
+                    </Dialog>
+                  </Button>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <Tables data={kompos} />
+              </CardContent>
+            </Card>
+          </TabsContent>
 
-					{/* KOMPOS */}
-					<TabsContent value="kompos">
-						<Card className={sty.Card}>
-							<CardHeader className="grid grid-cols-2">
-								<div>
-									<CardTitle>Kompos</CardTitle>
-									<CardDescription>
-										Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, labore.
-									</CardDescription>
-								</div>
-								<div className="flex justify-end gap-4">
-									<Button variant="outline" size="icon" className={sty.Button}>
-										<Dialog>
-											<DialogTrigger asChild>
-												<PlusIcon />
-											</DialogTrigger>
-											<DialogContent className="sm:max-w-[425px]">
-												<InsightCard>Kompos</InsightCard>
-											</DialogContent>
-										</Dialog>
-									</Button>
-								</div>
-							</CardHeader>
-							<CardContent>
-								<Tables />
-							</CardContent>
-						</Card>
-					</TabsContent>
+          {/* ORGANIK */}
+          <TabsContent value="organik">
+            <Card className={sty.Card}>
+              <CardHeader className="grid grid-cols-2">
+                <div>
+                  <CardTitle>Organik</CardTitle>
+                  <CardDescription className="mt-4">
+                    <p>{`Total Organik: ${data.totalOrganic}`}</p>
+                  </CardDescription>
+                </div>
+                <div className="flex justify-end gap-4">
+                  <Button variant="outline" size="icon" className={sty.Button}>
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <PlusIcon />
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <InsightCard>Organik</InsightCard>
+                      </DialogContent>
+                    </Dialog>
+                  </Button>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <Tables data={organik} />
+              </CardContent>
+            </Card>
+          </TabsContent>
 
-					{/* ORGANIK */}
-					<TabsContent value="organik">
-						<Card className={sty.Card}>
-							<CardHeader className="grid grid-cols-2">
-								<div>
-									<CardTitle>Organik</CardTitle>
-									<CardDescription>
-										Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, labore.
-									</CardDescription>
-								</div>
-								<div className="flex justify-end gap-4">
-									<Button variant="outline" size="icon" className={sty.Button}>
-										<Dialog>
-											<DialogTrigger asChild>
-												<PlusIcon />
-											</DialogTrigger>
-											<DialogContent className="sm:max-w-[425px]">
-												<InsightCard>Organik</InsightCard>
-											</DialogContent>
-										</Dialog>
-									</Button>
-								</div>
-							</CardHeader>
-							<CardContent>
-								<Tables />
-							</CardContent>
-						</Card>
-					</TabsContent>
-
-					{/* ANORGANIK */}
-					<TabsContent value="anorganik">
-						<Card className={sty.Card}>
-							<CardHeader className="grid grid-cols-2">
-								<div>
-									<CardTitle>Anorganik</CardTitle>
-									<CardDescription>
-										Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, labore.
-									</CardDescription>
-								</div>
-								<div className="flex justify-end gap-4">
-									<Button variant="outline" size="icon" className={sty.Button}>
-										<Dialog>
-											<DialogTrigger asChild>
-												<PlusIcon />
-											</DialogTrigger>
-											<DialogContent className="sm:max-w-[425px]">
-												<InsightCard>Anorganik</InsightCard>
-											</DialogContent>
-										</Dialog>
-									</Button>
-								</div>
-							</CardHeader>
-							<CardContent>
-								<Tables />
-							</CardContent>
-						</Card>
-					</TabsContent>
-				</Tabs>
-			</div>
-		</>
-	);
+          {/* ANORGANIK */}
+          <TabsContent value="anorganik">
+            <Card className={sty.Card}>
+              <CardHeader className="grid grid-cols-2">
+                <div>
+                  <CardTitle>Anorganik</CardTitle>
+                  <CardDescription className="mt-4">
+                    <p>{`Total Anorganik: ${data.totalAnorganic}`}</p>
+                  </CardDescription>
+                </div>
+                <div className="flex justify-end gap-4">
+                  <Button variant="outline" size="icon" className={sty.Button}>
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <PlusIcon />
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <InsightCard>Anorganik</InsightCard>
+                      </DialogContent>
+                    </Dialog>
+                  </Button>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <Tables data={anorganik} />
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
 }
