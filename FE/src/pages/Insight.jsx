450a412dc22907a3a91bc0af8f149dc4677dd6ba
Numenor08 <athaya120@gmail.com>
2024-04-28 04:14:27 +0700

    fix insight

--- FE/src/pages/Insight.jsx
+++ FE/src/pages/Insight.jsx
@@ -22,13 +22,13 @@ const sty = {
 
 export default function Insight() {
   const { data } = useGet("http://127.0.0.1:8000/api/history/");
-  const kompos = data.data.filter(
+  const kompos = data.history.filter(
     (item) => item.tipe_sampah.toLowerCase() === "kompos"
   );
-  const organik = data.data.filter(
+  const organik = data.history.filter(
     (item) => item.tipe_sampah.toLowerCase() === "organik"
   );
-  const anorganik = data.data.filter(
+  const anorganik = data.history.filter(
     (item) => item.tipe_sampah.toLowerCase() === "anorganik"
   );
   return (
